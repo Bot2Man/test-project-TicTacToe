9c8d8320e0cfacffa30d2f21a5def602517c7130
gedivo <g.vollono@reply.it>
2025-09-11 18:18:42 +0200

    project reassessment

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
+//calling elements
 const gameSquares = document.querySelectorAll(".square");
+
+//adding click to squares
 for (let i = 0; i < gameSquares.length; i++) {
   let clickSquare = gameSquares[i];
+  clickSquare.onclick = playGame;
 }
+
+//gameplay
+function playGame() {}
