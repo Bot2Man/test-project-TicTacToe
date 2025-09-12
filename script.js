9adeb47537b2b25dc0f3a668bffa7180b8fba82b
gedivo <g.vollono@reply.it>
2025-09-12 18:18:11 +0200

    redoing javascript

--- script.js
+++ script.js
@@ -1,5 +1,30 @@
 //calling elements
+const squareX = document.querySelector(".box-x");
+const squareO = document.querySelector(".box-o");
+const modal = document.querySelector(".modal");
+const choices = document.querySelector(".choice");
 const gameSquares = document.querySelectorAll(".square");
+let playerSymbol;
+let pcSymbol;
+
+//gameplay
+function startGame(symbol) {
+  for (let i = 0; i < choices.length; i++) {
+    let chosen = choices[i];
+  }
+}
+
+//adding click to player choice
+squareX.addEventListener("click", function () {
+  playerSymbol = "x";
+  pcSymbol = "o";
+  modal.style.display = "none";
+});
+squareO.addEventListener("click", function () {
+  pcSymbol = "x";
+  playerSymbol = "o";
+  modal.style.display = "none";
+});
 
 //adding click to squares
 for (let i = 0; i < gameSquares.length; i++) {
