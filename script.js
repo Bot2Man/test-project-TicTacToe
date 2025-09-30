9b2d55ce06bc48af3f185f492e9b8323c9bf4e4d
Bot2Man <bot2manvt@gmail.com>
2025-09-30 08:21:03 +0200

    made some changes

--- script.js
+++ script.js
@@ -4,14 +4,17 @@ const squareO = document.querySelector(".box-o");
 const modal = document.querySelector(".modal");
 const choices = document.querySelector(".choice");
 const gameSquares = document.querySelectorAll(".square");
+let currentTurn = squareX;
+let gameOver = false;
+let boardStatus = ["", "", "", "", "", "", "", "", ""];
 let playerSymbol;
 let pcSymbol;
 
 //gameplay
-function startGame(symbol) {
-  for (let i = 0; i < choices.length; i++) {
-    let chosen = choices[i];
-    chosen.onclick = playGame;
+function startGame() {
+  for (let i = 0; i < gameSquares.length; i++) {
+    let clickSquare = gameSquares[i];
+    clickSquare.onclick = playGame;
   }
 }
 
@@ -20,18 +23,14 @@ squareX.addEventListener("click", function () {
   playerSymbol = "x";
   pcSymbol = "o";
   modal.style.display = "none";
+  startGame();
 });
 squareO.addEventListener("click", function () {
   pcSymbol = "x";
   playerSymbol = "o";
   modal.style.display = "none";
+  startGame();
 });
-
-//adding click to squares
-for (let i = 0; i < gameSquares.length; i++) {
-  let clickSquare = gameSquares[i];
-  clickSquare.onclick = playGame;
-}
 
 //gameplay
 function playGame() {}
